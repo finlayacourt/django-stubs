c113072572494a0f2efd14d0bfb29eb0a80631e2
Finlay A’Court <finlay@netfm.org>
2025-08-27 11:00:44 +0100

    pyright: HttpRequestUser

--- django-stubs/http/request.pyi
+++ django-stubs/http/request.pyi
@@ -3,7 +3,7 @@ from collections.abc import Awaitable, Callable, Iterable, Iterator, Mapping, Se
 from re import Pattern
 from typing import Any, BinaryIO, Literal, NoReturn, TypeAlias, TypeVar, overload, type_check_only
 
-from django.contrib.auth.models import _AnyUser
+from django.contrib.auth.models import _AnyUser, _User
 from django.contrib.sessions.backends.base import SessionBase
 from django.contrib.sites.models import Site
 from django.core.files import uploadedfile, uploadhandler
@@ -116,6 +116,10 @@ class HttpRequest:
 class _MutableHttpRequest(HttpRequest):
     GET: QueryDict  # type: ignore[assignment]
     POST: QueryDict  # type: ignore[assignment]
+
+@type_check_only
+class HttpRequestUser(HttpRequest):
+    user: _User
 
 _Z = TypeVar("_Z")
 
